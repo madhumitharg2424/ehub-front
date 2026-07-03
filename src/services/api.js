26770517ae9e8ecbf1e93e7861044d56f2561ec8
MADHU <madhumitha0836@gmail.com>
2026-07-03 13:13:37 +0530

    Connected frontend to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://eventhub-backend-7oj3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
